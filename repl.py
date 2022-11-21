9e3b787bce5ed09d2f20883d7533e70385aff2ba
Eduard K <edvvak97@proton.me>
2022-11-22 02:03:13 +0300

    Added threads

--- repl.py
+++ repl.py
@@ -1,4 +1,6 @@
 from ctypes import *
+from threading import Thread
+
 import json
 import sys
 import math
@@ -25,28 +27,16 @@ client_id = _td_create_client_id()
 
 ###		Main cycle
 
-while True:
+line = ''
+updatesFilter = ''
+keepWork = True
 
-	line = input('json> ')
-	
-	try:
-
-		data = json.loads(line)
-	
-	except:
-
-		print('Invalid json, try again.')
-
-		continue
-
-	data['@extra'] = 1
-
-	_td_send(client_id, json.dumps(data).encode('utf-8'))
-
+def listenToTDLibUpdates():
 	while True:
-
+		
+		if keepWork == False:
+			break
 		msg = _td_receive(math.inf).decode('utf-8')
-
 		data = json.loads(msg.encode('utf-8'))
 		
 		if  ("@extra" in data):
@@ -55,4 +45,25 @@ while True:
 				
 				print(("{}:\n{}".format("Look up for @extra", msg)))
 
-				break
+tListener = Thread(target = listenToTDLibUpdates)
+
+while (True):
+	#updatesFilter = input('@type = ')
+	#if updatesFilter == "exit":
+		#quit()
+	line = input('json> ')
+	if (line == 'exit'):
+		keepWork = False
+		tListener.join()
+		quit()
+
+	try:
+		data = json.loads(line)
+	except:
+		print('Invalid json, try again.')
+		continue
+
+	data['@extra'] = 1
+	_td_send(client_id, json.dumps(data).encode('utf-8'))
+	if tListener.is_alive() == False:
+		tListener.start()
